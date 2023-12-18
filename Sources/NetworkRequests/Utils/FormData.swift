3f779bd50a9f9a61fc8b897e5c51fa608d269b0b
ikok07 <kokmarok@gmail.com>
2023-12-18 21:12:47 +0200

    fix form data

--- Sources/NetworkRequests/Utils/FormData.swift
+++ Sources/NetworkRequests/Utils/FormData.swift
@@ -9,23 +9,17 @@ import UIKit
 import JSONCoder
 
 public extension Request {
-    static func patchFormData<T: Codable>(url: String, json: Data? = nil, images: [UIImage?], authToken: String? = nil) async -> Result<T, NetworkError> {
+    static func patchFormData<T: Codable>(url: String, json: Data? = nil, image: UIImage? = nil, authToken: String? = nil) async -> Result<T, NetworkError> {
         
         let boundary = FormData.generateBoundary()
-        var imagesData: [Data] = []
-        
-        for image in images {
-            if let image {
-                imagesData.append(ImageMedia(withImage: image, key: "image").data!)
-            }
-        }
+        let imageData = ImageMedia(withImage: image, key: "image").data
         
         var request = URLRequest(url: URL(string: url)!)
         request.httpMethod = "PATCH"
         request.setValue("multipart/form-data; boundary=\(boundary)", forHTTPHeaderField: "Content-Type")
         request.setValue("Bearer \(authToken ?? "")", forHTTPHeaderField: "Authorization")
-        request.httpBody = FormData.createBody(json: json, images: imagesData, boundary: boundary)
-        print("REQUEST DATA: \(String(data: FormData.createBody(json: json, images: imagesData, boundary: boundary), encoding: .utf8))")
+        request.httpBody = FormData.createBody(json: json, image: imageData, boundary: boundary)
+        print("REQUEST DATA: \(String(data: FormData.createBody(json: json, image: imageData, boundary: boundary), encoding: .utf8))")
         
         do {
             let (data, _) = try await URLSession.shared.data(for: request)
@@ -49,7 +43,7 @@ public struct FormData {
         return "Boundary-\(UUID().uuidString)"
     }
     
-    static func createBody(json: Data?, images: [Data]?, boundary: String) -> Data {
+    static func createBody(json: Data?, image: Data?, boundary: String) -> Data {
         var body = Data()
         
         if let json = json {
@@ -58,21 +52,18 @@ public struct FormData {
             body.append("Content-Type: application/json\r\n\r\n".data(using: .utf8)!)
             body.append(json)
         }
-
-        if let images {
-            for imageData in images {
-                body.append("--\(boundary)\r\n".data(using: .utf8)!)
-                body.append("Content-Disposition: form-data; name=\"photo\"; filename=\"image.jpg\"\r\n".data(using: .utf8)!)
-                body.append("Content-Type: image/jpeg\r\n\r\n".data(using: .utf8)!)
-                body.append(imageData)
-            }
+        
+        if let image = image {
+            body.append("\r\n--\(boundary)\r\n".data(using: .utf8)!)
+            body.append("Content-Disposition: form-data; name=\"photo\"; filename=\"image.jpg\"\r\n".data(using: .utf8)!)
+            body.append("Content-Type: image/jpeg\r\n\r\n".data(using: .utf8)!)
+            body.append(image)
         }
         
         body.append("\r\n--\(boundary)--\r\n".data(using: .utf8)!)
         
-        print(String(data: body, encoding: .utf8))
-        
         return body
+        
     }
     
 }
